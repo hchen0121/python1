35317e78e41bb41c143a407059eda9461ce6506e
Han Chen <Han_Chen@cpr.ca>
2019-11-24 14:13:49 -0700

    numpy meetup

--- sample2.py
+++ sample2.py
@@ -1,2 +1,12 @@
-print('hello world')
-print('hello world')
+import numpy as np
+
+x_list = [1, 2, 3, 4]
+y_list = [5, 6, 7, 8]
+print(x_list+y_list)
+
+v = np.array([9, 10])
+w = np.array([11, 12])
+print(v)
+print(v.dot(w))
+print(np.sum(w))
+
